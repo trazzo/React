33a8562c15141e416d3b9df348c417f8ccdb1a1b
Rico <manuelricoroot@gmail.com>
2019-08-05 21:39:53 +0200

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle,Breadcrumb, BreadcrumbItem, Button, Label, Modal, ModalHeader, ModalBody, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -131,7 +132,25 @@ class CommentForm extends Component {
     }
 
     const DishDetail = (props) => {
-        if (props.dish != null) {
+        if (props.isLoaging) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,8 +1,26 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionTypes from './ActionTypes';
 
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (state = {
+        isLoading: true,
+        errMess: null,
+        dishes: []
+    }, action) => {
     switch(action.type) {
+        case ActionTypes.ADD_DISHES:
+            return {...state, isLoading: false, errMess: null, dishes: action.payload};
+
+        case ActionTypes.DISHES_LOADING:
+            /* {...state, whatever} => indicates you can pass the actual state (no matter 
+            what value it is having) and add to it the next properties in this case:
+            - isLoading
+            - errMess 
+            - dishes
+            returning a copy of the original state getting the new values without mutating it */
+            return {...state, isLoading: true, errMess: null, dishes: []};
+        case ActionTypes.DISHES_FAILED:
+            return {...state, isLoading: false, errMess: action.payload, dishes: []};
+
         default:
             return state;
     }
-}
+};
